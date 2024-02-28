838377352781388b9743009993531579f9eb3cd2
Haoran Li (Evan) <65979806+Haoranli802@users.noreply.github.com>
2024-02-28 09:12:32 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,16 @@
+// LC 344
+class Solution {
+    public void reverseString(char[] s) {
+        if(s.length == 1) return;
+        int left = 0; 
+        int right = s.length - 1;
+        while(left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left ++;
+            right --;
+        }
+    }
+}
+// Time: O(N), Space: O(1)
