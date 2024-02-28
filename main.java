9340b7bce0ec6e4d19c0944d49c32841bf8405e7
Haoran Li (Evan) <65979806+Haoranli802@users.noreply.github.com>
2024-02-28 09:27:16 +0800

    Update main.java

--- main.java
+++ main.java
@@ -60,3 +60,22 @@ class Solution {
 }
 
 // Time: O(N), Space: O(N)
+
+
+// 卡码 54
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                sb.append("number");
+            }else sb.append(s.charAt(i));
+        }
+        System.out.println(sb);
+    }
+}
