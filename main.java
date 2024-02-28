c2a658e06765de9289e307770e4a6b65850b690c
Haoran Li (Evan) <65979806+Haoranli802@users.noreply.github.com>
2024-02-28 09:59:35 +0800

    Update main.java

--- main.java
+++ main.java
@@ -141,3 +141,36 @@ class Solution {
 }
 
 // Time: O(N), Space: O(N)
+
+// 卡码55
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = Integer.parseInt(in.nextLine());
+        String s = in.nextLine();
+
+        int len = s.length();  //获取字符串长度
+        char[] chars = s.toCharArray();
+        reverseString(chars, 0, len - n - 1);  //反转前一段字符串，此时的字符串首尾是0,len - n - 1
+        reverseString(chars, len - n, len - 1);  //反转后一段字符串，此时的字符串首尾是len - n,len - 1
+        reverseString(chars, 0, len - 1);  //反转整个字符串
+
+        System.out.println(chars);
+
+    }
+
+    public static void reverseString(char[] ch, int start, int end) {
+        //异或法反转字符串，参照题目 344.反转字符串的解释
+        while (start < end) {
+            ch[start] ^= ch[end];
+            ch[end] ^= ch[start];
+            ch[start] ^= ch[end];
+            start++;
+            end--;
+        }
+    }
+}
+// Time: O(N), Space: O(N)
