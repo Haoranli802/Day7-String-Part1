36590480e9939f9dc8d278f41c532e831ca1cb52
Haoran Li (Evan) <65979806+Haoranli802@users.noreply.github.com>
2024-02-28 09:19:19 +0800

    Update main.java

--- main.java
+++ main.java
@@ -14,3 +14,25 @@ class Solution {
     }
 }
 // Time: O(N), Space: O(1)
+
+// LC 541
+
+class Solution {
+    public String reverseStr(String s, int k) {
+        char[] str = s.toCharArray();
+        for(int i = 0; i < str.length; i += 2 * k){
+            int left = i;
+            int right = Math.min(str.length - 1,left + 
+             k - 1);
+            while(left < right){
+                char temp = str[left];
+                str[left] = str[right];
+                str[right] = temp;
+                left ++;
+                right --;
+            }
+        }
+        return new String(str);
+    }
+}
+// Time: O(N), Space: O(N)
